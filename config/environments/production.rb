db0230d3f7677c8c1520959202aa02aeba104678
yasu.miyazaki <kizayami.yasu@gmail.com>
2018-07-09 01:45:27 +0000

    mailer in production (production.rb)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,17 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sodone-proto_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  
+  # Email configuration on 20180706
+  config.action_mailer.default_url_options = { host: 'www.sodone.jp' }
+  
+  # Email configuration on 20180705
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["EMAIL_HIDDEN"],
+    :password => ENV["PASSWORD_HIDDEN"],
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
